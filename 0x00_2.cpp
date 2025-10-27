ed5003f43a22fa4f189e9da2051a08c9b85343b5
SANGJUNSEO <ssjjnn01@gachon.ac.kr>
2025-10-27 16:46:12 +0900

    vector

--- 0x00_2.cpp
+++ 0x00_2.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-int main(){
-    
+void func1(vector<int> v) {
+    v[10] = 7;
+}
+
+int main(void){
+    vector<int> v(100);
+    func1(v);
+    cout << v[10];
 }
