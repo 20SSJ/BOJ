224b5f8c7e9874d7d74329cdd33621327c56f87e
SANGJUNSEO <ssjjnn01@gachon.ac.kr>
2025-12-19 16:55:02 +0900

    보물(정렬)

---
+++ 1026.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n, e, result, total;
+vector<int> a;
+vector<int> b;
+int main(){
+    cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        cin >> e;
+        a.push_back(e);
+    }
+    for(int i = 0; i < n; i++){
+        cin >> e;
+        b.push_back(e);
+    }
+    sort(a.begin(), a.end());
+    sort(b.rbegin(), b.rend());
+    for(int i = 0; i < n; i++){
+        result = a[i] * b[i];
+        total += result;
+    }
+    cout << total;
+    return 0;
+}
